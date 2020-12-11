1ec619780e185e05c2c91f9fb94aec057a946ee2
Tasif Tarikul <tasiftarikul@gmail.com>
2020-12-11 20:13:49 +0800

    first test

--- src/UserApp/tests.py
+++ src/UserApp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Order, User
 
-# Create your tests here.
+
+class OrderModelTest(TestCase):
+
+    def test_update_file_name(self):
+        order = Order(id=23124, order_type="cybershop", package_image="imagename.jpg")
+        self.assertEqual(
+            order.update_filename("imagename.jpg"), "order_images/cybershop/23124imagename.jpg",
+            "order filename not Equal")
+
+
+class UserModelTest(TestCase):
+
+    def test_profile_pic_folder(self):
+        user = User(id=2, profile_pic="propic.jpg")
+        self.assertEqual(user.profile_pic_folder("propic.jpg"), "user_profile_images/2/propic.jpg")
